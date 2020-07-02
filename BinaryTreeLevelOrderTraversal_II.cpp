2a765f01f35da246027b7f88369d1cb904093832
Ahmad Radmehr <radmehr@vt.edu>
2020-07-02 09:40:01 -0400

    "Binary Tree Level Order Traversal II" Added Second Solution

--- BinaryTreeLevelOrderTraversal_II.cpp
+++ BinaryTreeLevelOrderTraversal_II.cpp
@@ -27,43 +27,73 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class LevelOrderBottom {
+//class LevelOrderBottom {
+//public:
+//    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+//        if (!root) return vector<vector<int>>();
+//        vector<vector<int>> vv;
+//        vector<int> v;
+//        int n {1}, current {0}, next {0};
+//        v.push_back(root->val);
+//        vv.push_back(v);
+//        v.clear();
+//        std::queue<TreeNode*> q;
+//        q.push(root->left);
+//        q.push(root->right);
+//        if (root->left && root->right)
+//            current = 2;
+//        else if ((root->left && !root->right) || (!root->left && root->right))
+//            current = 1;
+//        while (!q.empty()) {
+//            TreeNode* p = q.front();
+//            q.pop();
+//            if (!p) continue;
+//            v.push_back(p->val);
+//            q.push(p->left);
+//            q.push(p->right);
+//            current--;
+//            if (p->left && p->right)
+//                next += 2;
+//            else if ((p->left && !p->right) || (!p->left && p->right))
+//                next++;
+//            if (current == 0) {
+//                vv.insert(vv.begin(), v);
+//                v.clear();
+//                current = next;
+//                next = 0;
+//                n++;
+//            }
+//        }
+//        return vv;
+//    }
+//};
+
+class LevelOrderBotto {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        if (!root) return vector<vector<int>>();
-        vector<vector<int>> vv;
-        vector<int> v;
-        int n {1}, current {0}, next {0};
-        v.push_back(root->val);
-        vv.push_back(v);
-        v.clear();
-        std::queue<TreeNode*> q;
-        q.push(root->left);
-        q.push(root->right);
-        if (root->left && root->right)
-            current = 2;
-        else if ((root->left && !root->right) || (!root->left && root->right))
-            current = 1;
+        if (!root)
+            return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<vector<int>> res;
+        unordered_map<TreeNode*, int> level;
+        level[root] = 0;
         while (!q.empty()) {
-            TreeNode* p = q.front();
+            TreeNode* t = q.front();
             q.pop();
-            if (!p) continue;
-            v.push_back(p->val);
-            q.push(p->left);
-            q.push(p->right);
-            current--;
-            if (p->left && p->right)
-                next += 2;
-            else if ((p->left && !p->right) || (!p->left && p->right))
-                next++;
-            if (current == 0) {
-                vv.insert(vv.begin(), v);
-                v.clear();
-                current = next;
-                next = 0;
-                n++;
+            if (level[t] == res.size())
+                res.insert(res.begin(), {t->val});
+            else
+                res.front().push_back(t->val);
+            if (t->left) {
+                q.push(t->left);
+                level[t->left] = level[t] + 1;
+            }
+            if (t->right) {
+                q.push(t->right);
+                level[t->right] = level[t] + 1;
             }
         }
-        return vv;
+        return res;
     }
 };
