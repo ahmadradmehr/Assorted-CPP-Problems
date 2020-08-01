7fa60ca8bd8829a34c938c2054ce2c26e483a1f6
Ahmad Radmehr <radmehr@vt.edu>
2020-08-01 19:54:31 -0400

    "Reverse Integer" Updated Solution - 200801

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -18,22 +18,35 @@
  * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
  * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  */
- 
-#include <math.h>
+
 class ReverseInteger {
 public:
     int reverse(int x) {
-        long rev {0};
-        long maxLimit = pow(2, 31)-1;
-        long minLimit = -pow(2, 31);
+        int num {0};
         while (x != 0) {
-            if (rev * 10 + x % 10 <= maxLimit && 
-                rev * 10 + x %10 >= minLimit) {
-                rev = rev * 10 + x % 10;
-                x /= 10;
-            } else
+            if (x > 0 && INT_MAX/10 < num || x < 0 && INT_MIN/10 > num)
                 return 0;
+            num = num * 10 + x % 10;
+            x /= 10;
         }
-        return rev;
+        return num;
     }
 };
+
+//class ReverseInteger {
+//public:
+//    int reverse(int x) {
+//        long rev {0};
+//        long maxLimit = pow(2, 31)-1;
+//        long minLimit = -pow(2, 31);
+//        while (x != 0) {
+//            if (rev * 10 + x % 10 <= maxLimit && 
+//                rev * 10 + x %10 >= minLimit) {
+//                rev = rev * 10 + x % 10;
+//                x /= 10;
+//            } else
+//                return 0;
+//        }
+//        return rev;
+//    }
+//};
