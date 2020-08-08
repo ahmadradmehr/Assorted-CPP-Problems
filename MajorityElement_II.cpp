1c6b5acfc9d27560a2d1175f31c431b55e825485
Ahmad Radmehr <radmehr@vt.edu>
2020-08-08 05:50:17 -0400

    Create "Majority Element II" Solution

---
+++ MajorityElement_II.cpp
@@ -0,0 +1,50 @@
+/* Majority Element II
+ * 
+ * Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+ * 
+ * Note: The algorithm should run in linear time and in O(1) space.
+ * 
+ * Example 1:
+ * Input: [3,2,3]
+ * Output: [3]
+ * 
+ * Example 2:
+ * Input: [1,1,1,3,3,2,2,2]
+ * Output: [1,2]
+ */
+ 
+class MajorityElement_II {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> res;
+        int num1 {0}, num2 {1}, count1 {0}, count2 {0};
+        for (auto &n: nums) {
+            if (n == num1)
+                count1++;
+            else if (n == num2)
+                count2++;
+            else if (!count1) {
+                num1 = n;
+                count1 = 1;
+            } else if (!count2) {
+                num2 = n;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = count2 = 0;
+        for (auto &n: nums) {
+            if (n == num1)
+                count1++;
+            else if (n == num2)
+                count2++;
+        }
+        if (count1 > nums.size()/3)
+            res.push_back(num1);
+        if (count2 > nums.size()/3)
+            res.push_back(num2);
+        return res;
+    }
+};
