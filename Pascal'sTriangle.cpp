87b852d98ddec6813acdbf9a30b24b348b62fb71
Ahmad Radmehr <radmehr@vt.edu>
2020-08-18 15:32:07 -0400

    Update "Pascal's Triangle" Solution

--- Pascal'sTriangle.cpp
+++ Pascal'sTriangle.cpp
@@ -14,25 +14,44 @@
  * [1,4,6,4,1]
  * ]
  */
- 
+
 class PascalTriangle {
 public:
     vector<vector<int>> generate(int numRows) {
-        if (numRows == 0) return {};
-        int n {1};
-        vector<int> v;
-        vector<vector<int>> vv;
-        while (n <= numRows) {
-            for (int i {0}; i < n; i++) {
-                if (i == 0 || i == n-1)
-                    v.push_back(1);
-                else
-                    v.push_back(vv[n-2][i-1]+vv[n-2][i]);
+        if (numRows == 0)
+            return {};
+        vector<vector<int>> Pascal {{1}};
+        int n = 1;
+        while (n++ < numRows) {
+            vector<int> v (n, 1);
+            for (int i {1}; i < n - 1; i++) {
+                v.at(i) = Pascal.back().at(i-1) + Pascal.back().at(i);
             }
-            vv.push_back(v);
-            v.clear();
-            n++;
+            Pascal.push_back(v);
         }
-        return vv;
+        return Pascal;
     }
 };
+
+
+//class PascalTriangle {
+//public:
+//    vector<vector<int>> generate(int numRows) {
+//        if (numRows == 0) return {};
+//        int n {1};
+//        vector<int> v;
+//        vector<vector<int>> vv;
+//        while (n <= numRows) {
+//            for (int i {0}; i < n; i++) {
+//                if (i == 0 || i == n-1)
+//                    v.push_back(1);
+//                else
+//                    v.push_back(vv[n-2][i-1]+vv[n-2][i]);
+//            }
+//            vv.push_back(v);
+//            v.clear();
+//            n++;
+//        }
+//        return vv;
+//    }
+//};
