2d924d961fe84049a0937669fa92c849c295681d
Ahmad Radmehr <radmehr@vt.edu>
2020-09-06 08:35:43 -0400

    Create "First Unique Character in a String" Solution

---
+++ FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,27 @@
+/* First Unique Character in a String
+ * 
+ * Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+ * Examples:
+ * s = "leetcode"
+ * return 0.
+ * 
+ * s = "loveleetcode"
+ * return 2.
+ * 
+ * Note: You may assume the string contains only lowercase English letters.
+ */
+
+class FirstUniqueCharacter {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> m;
+        for (int i {0}; i < s.length(); i++) {
+            m[s.at(i)]++;
+        }
+        for (int i {0}; i < s.length(); i++) {
+            if (m[s.at(i)] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
