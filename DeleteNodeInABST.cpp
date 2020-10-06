ea9f9c537909d4b8ba7bc4a5c7f2ede308b156bd
Ahmad Radmehr <radmehr@vt.edu>
2020-10-06 14:25:57 -0400

    Create "Delete Node in a BST" Solution

---
+++ DeleteNodeInABST.cpp
@@ -0,0 +1,93 @@
+/*
+ * ### Delete Node in a BST ###
+ * 
+ * 
+ * Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+ * Basically, the deletion can be divided into two stages:
+ * Search for a node to remove.
+ * If the node is found, delete the node.
+ * Follow up: Can you solve it with time complexity O(height of tree)?
+ * 
+ * Example 1:
+ * Input: root = [5,3,6,2,4,null,7], key = 3
+ * Output: [5,4,6,2,null,null,7]
+ * Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+ * One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+ * Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+ * 
+ * Example 2:
+ * Input: root = [5,3,6,2,4,null,7], key = 0
+ * Output: [5,3,6,2,4,null,7]
+ * Explanation: The tree does not contain a node with value = 0.
+ * 
+ * Example 3:
+ * Input: root = [], key = 0
+ * Output: []
+ * 
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 104].
+ * -105 <= Node.val <= 105
+ * Each node has a unique value.
+ * root is a valid binary search tree.
+ * -105 <= key <= 105
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class DeleteNodeBST {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root)
+            return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        unordered_map<TreeNode*, TreeNode*> parent;
+        parent[root] = NULL;
+        while (!q.empty()) {
+            TreeNode* t = q.front();
+            q.pop();
+            if (t->val == key) {
+                if (!t->right) {
+                    if (!parent[t])
+                        return t->left;
+                    if (parent[t]->left == t)
+                        parent[t]->left = t->left;
+                    else
+                        parent[t]->right = t->left;
+                    return root;
+                }
+                TreeNode *l = t->left;
+                if (!parent[t])
+                    root = t->right;
+                else if (parent[t]->left == t)
+                    parent[t]->left = t->right;
+                else
+                    parent[t]->right = t->right;
+                t = t->right;
+                while (t->left)
+                    t = t->left;
+                t->left = l;
+                return root;
+            }
+            if (t->left) {
+                q.push(t->left);
+                parent[t->left] = t;
+            }
+            if (t->right) {
+                q.push(t->right);
+                parent[t->right] = t;
+            }
+        }
+        return root;
+    }
+};
